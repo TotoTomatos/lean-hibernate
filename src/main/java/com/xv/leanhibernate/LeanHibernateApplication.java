54f7e2cbe9abeff79fd1b5f623fc066cc8c6407c
Xu <1440930392@qq.com>
2024-05-29 15:44:40 +0800

    feat(构建): 项目初步构建
    项目依赖、项目配置、数据库文件

---
+++ src/main/java/com/xv/leanhibernate/LeanHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.xv.leanhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xv/leanhibernate/LeanHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xv.leanhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
